2b74ebc676ee6204cc9b85325af8cf7ef77db8cf
David gp <juliograjalesparedes127@gmail.com>
2023-09-15 19:15:33 +0200

    nuevas screen creadas y  archivo de barril actualizado

--- lib/presentation/screen/screen.dart
+++ lib/presentation/screen/screen.dart
@@ -1,3 +1,9 @@
-export  'package:widget_app/presentation/screen/buttons/buttons_screen.dart';
+export 'package:widget_app/presentation/screen/animated/animated_screen.dart';
+export 'package:widget_app/presentation/screen/app_tutorial/app_tutorial_screen.dart';
+export 'package:widget_app/presentation/screen/buttons/buttons_screen.dart';
 export 'package:widget_app/presentation/screen/cards/cards_screen.dart';
 export 'package:widget_app/presentation/screen/home/home_screen.dart';
+export 'package:widget_app/presentation/screen/infinite_scroll/infinit_scroll_screem.dart';
+export 'package:widget_app/presentation/screen/progress/progress_screen.dart';
+export 'package:widget_app/presentation/screen/snackbar/sbackbar_screen.dart';
+export 'package:widget_app/presentation/screen/ui_controls/ui_controls_screen.dart';
