f575629da29b34c3b6df399a0c0d596d2b7543a4
David gp <juliograjalesparedes127@gmail.com>
2023-09-18 20:26:02 +0200

    nuevas rutas añadidas para los appbar y tabbar

--- lib/config/router/router_app.dart
+++ lib/config/router/router_app.dart
@@ -1,5 +1,8 @@
 import 'package:go_router/go_router.dart';
+
+import 'package:widget_app/presentation/screen/app_bar/screens/screens.dart';
 import 'package:widget_app/presentation/screen/screen.dart';
+import 'package:widget_app/presentation/screen/tab_bar/screens/screens.dart';
 
 // GoRouter configuration
 final appRouter = GoRouter(
@@ -20,6 +23,64 @@ final appRouter = GoRouter(
       path: '/cards',
       name: CardsScreen.name,
       builder: (context, state) => const CardsScreen(),
+    ),
+  
+    //? AppBar Routes 
+
+    GoRoute(
+      path: '/appbar',
+      name: AppBarScreen.name,
+      builder: (context, state) => const AppBarScreen(),
+    ),
+
+    GoRoute(
+      path: '/appbar/one',
+      name: FirstAppBarScreen.name,
+      builder: (context, state) => const FirstAppBarScreen(),
+    ),
+
+    GoRoute(
+      path: '/appbar/two',
+      name: SecondAppBarScreen.name,
+      builder: (context, state) => const SecondAppBarScreen(),
+    ),
+
+    GoRoute(
+      path: '/appbar/three',
+      name: ThirdAppBarScreen.name,
+      builder: (context, state) => const ThirdAppBarScreen(),
+    ),
+
+    GoRoute(
+      path: '/appbar/fourth',
+      name: FourthAppBarScreen.name,
+      builder: (context, state) => const FourthAppBarScreen(),
+    ),
+
+    //? TabBar Routes
+
+    GoRoute(
+      path: '/tabbar',
+      name: TabBarScreen.name,
+      builder: (context, state) => const TabBarScreen(),
+    ),
+
+    GoRoute(
+      path: '/tabbar/one',
+      name: FirstTabBarScreen.name,
+      builder: (context, state) => const FirstTabBarScreen(),
+    ),
+
+    GoRoute(
+      path: '/tabbar/two',
+      name: SecondTabBarScreen.name,
+      builder: (context, state) => const SecondTabBarScreen(),
+    ),
+
+    GoRoute(
+      path: '/tabbar/three',
+      name: ThirdTabBarScreen.name,
+      builder: (context, state) => const ThirdTabBarScreen(),
     ),
   ],
 );
