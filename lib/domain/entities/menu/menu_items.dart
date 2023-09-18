7e07f3efb72aa2727a644ae7c54444c064854856
David gp <juliograjalesparedes127@gmail.com>
2023-09-18 20:25:25 +0200

    nuevos menus para los screen appbar y tabbar

--- lib/domain/entities/menu/menu_items.dart
+++ lib/domain/entities/menu/menu_items.dart
@@ -29,4 +29,72 @@ const appMenuItems = <MenuItem> [
       link: '/cards',
       icon: Icons.credit_card
   ),
+
+  MenuItem(
+    title: 'App Bar',
+    subTitle: 'Una Barra de aplicaciones',
+    link: '/appbar',
+    icon: Icons.app_shortcut_outlined
+  ),
+
+  MenuItem(
+    title: 'Tab Bar',
+    subTitle: 'Una Barra de pestañas principal',
+    link: '/tabbar',
+    icon: Icons.table_chart
+  ),
+];
+
+const appBarItems = <MenuItem>[
+  MenuItem(
+    title: 'AppBar one',
+    subTitle: 'a simple AppBar',
+    link:'/appbar/one',
+    icon: Icons.do_not_disturb_on_total_silence_rounded
+  ),
+
+  MenuItem(
+    title: 'AppBar two',
+    subTitle: 'a AppBar with actions',
+    link:'/appbar/two',
+    icon: Icons.do_not_disturb_on_total_silence_rounded
+  ),
+
+  MenuItem(
+    title: 'AppBar three',
+    subTitle: 'a AppBar with the propieties ShadowColor and scrollUnderElevation',
+    link:'/appbar/three',
+    icon: Icons.do_not_disturb_on_total_silence_rounded
+  ),
+
+  
+  MenuItem(
+    title: 'AppBar fourth',
+    subTitle: 'a AppBar with notification of scroll',
+    link:'/appbar/fourth',
+    icon: Icons.do_not_disturb_on_total_silence_rounded
+  ),
+];
+
+const tabBarItems = <MenuItem>[
+  MenuItem(
+    title: 'TabBar one',
+    subTitle: 'a tabBar using the widget  DefaultTabController to control the tabs, we are also making use of the isScrollable property of the TabBar widget',
+    link:'/tabbar/one',
+    icon: Icons.do_not_disturb_on_total_silence_rounded
+  ),
+
+  MenuItem(
+    title: 'TabBar two',
+    subTitle: 'TabBar nested',
+    link:'/tabbar/two',
+    icon: Icons.do_not_disturb_on_total_silence_rounded
+  ),
+
+  MenuItem(
+    title: 'TabBar three',
+    subTitle: 'TabBar controlled with a controller',
+    link:'/tabbar/three',
+    icon: Icons.do_not_disturb_on_total_silence_rounded
+  ),
 ];
