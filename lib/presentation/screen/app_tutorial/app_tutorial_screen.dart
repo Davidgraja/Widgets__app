332962e2436921bcc7956ac7bdf0511bd7ab30f6
David gp <juliograjalesparedes127@gmail.com>
2023-09-21 17:59:30 +0200

    layout de tutorial finalizado

--- lib/presentation/screen/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screen/app_tutorial/app_tutorial_screen.dart
@@ -29,6 +29,28 @@ class AppTutorialScreen extends StatefulWidget {
 
 class _AppTutorialScreenState extends State<AppTutorialScreen> {
 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body:LayoutBuilder(
+        builder: (context, constraints) => constraints.maxWidth > 360 ? const _SlideHorizontalLayout() : const  _SlideVerticalLayout() 
+      )
+    );
+  }
+}
+
+
+class _SlideHorizontalLayout extends StatefulWidget {
+  const _SlideHorizontalLayout();
+
+  @override
+  State<_SlideHorizontalLayout> createState() => _SlideHorizontalLayoutState();
+}
+
+class _SlideHorizontalLayoutState extends State<_SlideHorizontalLayout> {
+
   final PageController pageController = PageController();
   bool endReached = false;
 
@@ -50,59 +72,173 @@ class _AppTutorialScreenState extends State<AppTutorialScreen> {
   void dispose() {
     pageController.dispose();
     super.dispose();
-    
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          PageView(
-            controller:  pageController,
-            physics: const BouncingScrollPhysics(),
-            children: slides.map((slideData) => _Slide(title: slideData.title, caption: slideData.caption, imageUrl: slideData.imageUrl)).toList(),
-          ),
-
-          Positioned(
-            top: 40.0,
-            right: 20.0,
-            child: TextButton(
-              onPressed: (){
-                context.pop();
-              },
-              child: const Text('Salir'),
-            )
-          ),
-
-          endReached ?  
-          Positioned(
-            bottom: 30.0,
-            right: 20.0,
-            child: FadeInRight(
-              from: 15,
-              delay: const Duration(seconds: 1),
-              child: FilledButton(
-                onPressed: () => context.pop(),
-                child: const Text('Comenzar'),
-              ),
-            )
+    return Stack(
+      children: [
+        PageView(
+          controller: pageController,
+          physics: const BouncingScrollPhysics(),
+          children: slides.map((slide) => _SlideHorizontal(title: slide.title, caption: slide.caption, imageUrl: slide.imageUrl) ).toList()   
+        ),
+        Positioned(
+          top: 40,
+          right: 20,
+          child: TextButton(
+            onPressed: (){
+              context.pop();
+            },
+            child: const Text('Salir'),
           )
-          : const SizedBox()
-        ],
-      ) ,
+        ),
+
+       endReached ?
+        Positioned(
+          bottom: 30.0,
+          right: 20.0,
+          child: FadeInRight(
+            from: 15,
+            delay: const Duration(seconds: 1),
+            child: FilledButton(
+              onPressed: () => context.pop(),
+              child: const Text('Comenzar'),
+            ),
+          )
+        ) :  const SizedBox()
+        
+      ],
     );
   }
 }
 
-class _Slide extends StatelessWidget {
+
+class _SlideHorizontal extends StatelessWidget {
 
   final String title;
   final String caption;
   final String imageUrl;
 
-  const _Slide({required this.title, required this.caption, required this.imageUrl});
+  const _SlideHorizontal({required this.title, required this.caption, required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+
+    // Acceder a estilos de texto del tema general
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+    final size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Center(
+        child: Row(
+          children: [
+            // Image(image: AssetImage(imageUrl)),
+            Container( constraints: BoxConstraints(maxWidth:size.width * 0.5 ), child:Image.asset(imageUrl ,fit: BoxFit.cover),),
+           
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(title , style: titleStyle,),
+                const SizedBox(height: 10.0,),
+                SizedBox(
+                  width: size.width * 0.3,
+                  child: Text(caption , style: captionStyle,),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class _SlideVerticalLayout extends StatefulWidget {
+  const _SlideVerticalLayout();
+
+  @override
+  State<_SlideVerticalLayout> createState() => _SlideVerticalLayoutState();
+}
+
+class _SlideVerticalLayoutState extends State<_SlideVerticalLayout> {
+
+  
+  final PageController pageController = PageController();
+  bool endReached = false;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      final page = pageController.page ?? 0;
+
+      if(page >= (slides.length - 1.5)){
+        setState(() {
+          endReached = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+    children: [
+        PageView(
+          controller:  pageController,
+          physics: const BouncingScrollPhysics(),
+          children: slides.map((slideData) => _SlideVertical(title: slideData.title, caption: slideData.caption, imageUrl: slideData.imageUrl)).toList(),
+        ),
+
+        Positioned(
+          top: 40.0,
+          right: 20.0,
+          child: TextButton(
+            onPressed: (){
+              context.pop();
+            },
+            child: const Text('Salir'),
+          )
+        ),
+
+        endReached ?  
+        Positioned(
+          bottom: 30.0,
+          right: 20.0,
+          child: FadeInRight(
+            from: 15,
+            delay: const Duration(seconds: 1),
+            child: FilledButton(
+              onPressed: () => context.pop(),
+              child: const Text('Comenzar'),
+            ),
+          )
+        )
+        : const SizedBox()
+      ],
+    ); 
+  }
+}
+
+
+class _SlideVertical extends StatelessWidget {
+
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _SlideVertical({required this.title, required this.caption, required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
