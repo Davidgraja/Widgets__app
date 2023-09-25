05d4c85f84f06e0cad0a2e6c93491a17cd68d385
David gp <juliograjalesparedes127@gmail.com>
2023-09-25 02:27:53 +0200

    counter screen añadido

---
+++ lib/presentation/screen/counter/counter_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class CounterScreen extends StatelessWidget {
+
+  static const String name = 'counter_screen';
+
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
--- lib/presentation/screen/screen.dart
+++ lib/presentation/screen/screen.dart
@@ -1,4 +1,3 @@
-
 export 'package:widget_app/presentation/screen/animated/animated_screen.dart';
 export 'package:widget_app/presentation/screen/app_bar/app_bar_screen.dart';
 export 'package:widget_app/presentation/screen/app_tutorial/app_tutorial_screen.dart';
@@ -10,3 +9,4 @@ export 'package:widget_app/presentation/screen/progress/progress_screen.dart';
 export 'package:widget_app/presentation/screen/snackbar/sbackbar_screen.dart';
 export 'package:widget_app/presentation/screen/tab_bar/tab_bar_screen.dart';
 export 'package:widget_app/presentation/screen/ui_controls/ui_controls_screen.dart';
+export 'package:widget_app/presentation/screen/counter/counter_screen.dart';
