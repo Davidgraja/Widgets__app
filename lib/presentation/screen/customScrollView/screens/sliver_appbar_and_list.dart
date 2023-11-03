402878b2783bf9fe6a6256601678ec7bf0debf26
David Paredes <juliograjalesparedes127@gmail.com>
2023-11-03 15:47:05 +0100

    repaso de codigo en SliverAppBar

--- lib/presentation/screen/customScrollView/screens/sliver_appbar_and_list.dart
+++ lib/presentation/screen/customScrollView/screens/sliver_appbar_and_list.dart
@@ -10,34 +10,45 @@ class SliverAppbarAndList extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
-            backgroundColor: Colors.black87,
+            // backgroundColor: const Color.fromARGB(255, 0, 0, 0),
             foregroundColor: Colors.white,
             clipBehavior: Clip.hardEdge,
 
             //? deja fijo el appbar durante todo el scroll
-            // pinned: true,
+            pinned: true,
 
             //? genera un efecto flotante al realizar el scroll , funciona muy bien sin el flexibleSpace
-            floating: true,
+            floating:  true,
 
             /* 
             Si [snap] es verdadero, entonces genera un desplazamiento que expone la barra flotante de la aplicación,
             activará una animación que desliza toda la barra de la aplicación a la vista.
              */
-            // snap: true,
+            // snap: false,
             elevation: 4,
             stretch: true,
             actions: [
-              TextButton(onPressed: () {}, child: const Text('Actions')),
+              TextButton(onPressed: () {}, child: const Text('Actions' , style: TextStyle(color: Colors.white),)),
             ],
-            /* expandedHeight:500 ,
-            flexibleSpace: const FlexibleSpaceBar(
-              title: Text('SliverAppBar - FlexibleSpaceBar'),
-              // centerTitle: true,
-              titlePadding: EdgeInsets.all(10),
-              background: FlutterLogo(),
+            expandedHeight: 300  ,
+            flexibleSpace:   SizedBox(
+              width: double.infinity,
+              height: double.infinity,
+              child: Image.network(
+                  'https://img.freepik.com/foto-gratis/fondo-pantalla-abstracto-nebulosa-ultra-detallado-4_1562-749.jpg?size=626&ext=jpg&ga=GA1.2.386372595.1697587200&semt=sph',
+                  fit: BoxFit.cover,
+                ),
+            ),
 
-            )  */
+            /// podemos hacer uso del widget FlexibleSpaceBar , el cual nos dara un comportamiento particulas dentro del flexibleSpace
+            /* 
+             FlexibleSpaceBar(
+                background: Image.network(
+                'https://img.freepik.com/foto-gratis/fondo-pantalla-abstracto-nebulosa-ultra-detallado-4_1562-749.jpg?size=626&ext=jpg&ga=GA1.2.386372595.1697587200&semt=sph',
+                fit: BoxFit.cover,
+              ) ,
+            ) */  
+          
           ),
           SliverList(
             delegate: SliverChildBuilderDelegate(
@@ -45,7 +56,7 @@ class SliverAppbarAndList extends StatelessWidget {
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
                     color: Colors.blueAccent),
-                margin: const EdgeInsets.all(10),
+                margin: const EdgeInsets.only(top: 5 , left: 10 ,right: 10),
                 width: double.infinity,
                 height: 100,
                 child: Center(
