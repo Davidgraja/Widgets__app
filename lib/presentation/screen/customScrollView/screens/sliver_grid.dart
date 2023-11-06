06c284cbcd95695ed25585ab94c628caa10913f3
David Paredes <juliograjalesparedes127@gmail.com>
2023-11-06 10:06:39 +0100

    mejora de codigo realizada

--- lib/presentation/screen/customScrollView/screens/sliver_grid.dart
+++ lib/presentation/screen/customScrollView/screens/sliver_grid.dart
@@ -1,89 +1,69 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
+import 'package:flutter/material.dart';
 class CustomSliverGrid extends StatelessWidget {
+ 
   static const name = 'custom_sliver_grid';
   const CustomSliverGrid({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     return  Scaffold(
       body: SafeArea(
         child: CustomScrollView(
           slivers: [
+
+            const SliverAppBar(
+              title: Text('SliverGrid'),
+            ),
+
             SliverGrid(
-              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 250),
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 120),
               delegate: SliverChildBuilderDelegate(
-                childCount: 20,
-                (context, index) =>  Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                    alignment: Alignment.center,
-                    color: Colors.green.shade200 ,
-                    child: Text('SliverGrid : $index'),
-                  ),
-                )
+                childCount: 30,
+                (context, index) =>  _CustomSliverGrid(index: index , text: 'SliverGrid',)
               ),
             ),
 
             SliverGrid.count(
-              // crossAxisSpacing: 200,
-              // mainAxisSpacing: 400,
+              childAspectRatio: 3.0,
+              crossAxisSpacing: 2.0,
+              mainAxisSpacing: 2.0,
               crossAxisCount: 2,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                      alignment: Alignment.center,
-                      color: Colors.red.shade200 ,
-                      child: Text('SliverGrid.count'),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                      alignment: Alignment.center,
-                      color: Colors.red.shade200 ,
-                      child: Text('SliverGrid.count'),
-                  ),
-                ),
-              ],
+              children: List.generate(30, (index) => _CustomSliverGrid(index: index , text: 'SliverGrid count',))
             ),
 
-            SliverGrid.extent(
+            SliverGrid.extent(             
               maxCrossAxisExtent: 180,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                        alignment: Alignment.center,
-                        color: Colors.blue.shade200 ,
-                        child: Text('SliverGrid.extent'),
-                  ),
-                ),
-
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                        alignment: Alignment.center,
-                        color: Colors.blue.shade200 ,
-                        child: Text('SliverGrid.extent'),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                        alignment: Alignment.center,
-                        color: Colors.blue.shade200 ,
-                        child: Text('SliverGrid.extent'),
-                  ),
-                ),
-
-              ],
+              children: List.generate(30, (index) => _CustomSliverGrid(index: index , text: 'SliverGrid extent',))
             ),
   
           ],
         ),
       ),
+    );
+  }
+}
+
+class _CustomSliverGrid extends StatelessWidget {
+
+  final int index ;
+  final String? text;
+
+  const _CustomSliverGrid({ required this.index  ,  this.text});
+
+  @override
+  Widget build(BuildContext context) {
+  
+    final g = Random().nextInt(255);
+    final b = Random().nextInt(255);
+    final r = Random().nextInt(255);
+
+    return Container(
+        alignment: Alignment.center,
+        color: Color.fromRGBO(r, g, b, 1) ,
+        child: Text(text ?? '' , style: const TextStyle(color: Colors.white),),
     );
   }
 }
