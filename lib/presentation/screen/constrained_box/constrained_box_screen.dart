ca37009491faaa080678a08de89e15762f9af46a
David Paredes <juliograjalesparedes127@gmail.com>
2023-12-25 20:23:54 +0100

    construción con const añadido

--- lib/presentation/screen/constrained_box/constrained_box_screen.dart
+++ lib/presentation/screen/constrained_box/constrained_box_screen.dart
@@ -8,7 +8,7 @@ class ConstrainedBoxScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ConstrainedBox'),
+        title: const Text('ConstrainedBox'),
       ),
       body: const _ConstrainedView(),
     );
