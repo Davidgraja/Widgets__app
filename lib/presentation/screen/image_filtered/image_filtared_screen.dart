aaa2bb332337d724d74847bb122c2c757554ff81
David Paredes <juliograjalesparedes127@gmail.com>
2024-02-13 21:59:02 +0100

    ImageFIltered widget actualizado

--- lib/presentation/screen/image_filtered/image_filtared_screen.dart
+++ lib/presentation/screen/image_filtered/image_filtared_screen.dart
@@ -1,7 +1,10 @@
+
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
+/// con ImageFiltered podemos hacer varias cosas , la primera es aplicar un efecto de desenfoque , la segunda es palicar un efecto de transformación , estas son unas de las 
+/// muchas cosas que puede hacer este widget
 class ImageFilteredScreen extends StatelessWidget {
   static const name = 'image_filtered_screen';
   const ImageFilteredScreen({super.key});
@@ -31,6 +34,21 @@ class ImageFilteredScreen extends StatelessWidget {
               ),
               child: Image.network('https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjmvltxq_liOFQ5iLz7yFdL_zqywMQao2MKSdtHbLydxkTFpZI-tYkTw0x0CLVqKu_jKm_w9PnwY1rid7fsggYM5j8FI4gMOGfe_tIyFC0R0cB31oNSon__HqzyaT4lDmMDyyL3wFhODlDZIOuifjOLjY1AfDbR2_Y3-5E91euZWNeOEM0vnY94ChjY/s728-rw-ft-e30/android.png')
             ),
+
+
+            // Forma de transformar un widget con ImageFiltered
+
+            Align(
+              alignment: const Alignment(0.0 , 0.8),
+              child: ImageFiltered(
+                imageFilter: ImageFilter.matrix(
+                  Matrix4.rotationZ(0.4).storage
+                ),
+                child: const FlutterLogo(
+                  size: 150,
+                ),
+              )
+            )
           
           ],
         )
